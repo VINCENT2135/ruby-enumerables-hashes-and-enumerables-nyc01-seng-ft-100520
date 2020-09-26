434c0ede87c0d88041343dd336e812457c10a6dd
Vincent Ivos <vincentivos1@gmail.com>
2020-09-26 16:05:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,13 +5,5 @@
 # }
 
 def happy_birthday(birthday_kids)
-winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a &&
-    name.start_with?("A")
-     winner = name 
-   end
-   
- end
- winner
+
   # add your code snippet here!
